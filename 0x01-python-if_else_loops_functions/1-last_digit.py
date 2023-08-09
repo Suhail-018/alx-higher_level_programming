29c09fdf71f921dce593d71e456dab96b61f0ffc
root <suhailyksa@gmail.com>
2023-08-09 05:08:57 -0700

    another try fixing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-ls_dt = int(str(number)[-1]) 
+ls_dt = abs(number) % 10  # Get the last digit's absolute value
+
 if ls_dt > 5:
-  print(f"Last digit of {number} is {ls_dt} and is greater than 5")
+    print(f"Last digit of {number} is {ls_dt} and is greater than 5")
 elif ls_dt == 0:
     print(f"Last digit of {number} is {ls_dt} and is 0")
 else:
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(s):
+    result = ""
     for char in s:
         if 'a' <= char <= 'z':
-            print("{}".format(chr(ord(char) - ord('a') + ord('A'))), end="")
+            result += "{}".format(chr(ord(char) - ord('a') + ord('A')))
         else:
-            print("{}".format(char), end="")
-    print()
+            result += "{}".format(char)
+    print(result)
