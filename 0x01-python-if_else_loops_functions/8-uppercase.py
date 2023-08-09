a5430dee000bcce0e759205a851a704e91d39eb1
root <suhailyksa@gmail.com>
2023-08-09 04:59:08 -0700

    2nd deadline fix and sudmit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(s):
- for char in s:
-     if 'a' <= char <= 'z':
-            print(chr(ord(char) - ord('a') + ord('A')), end="")
-     else:
-            print(char, end="")
-print()  
+    for char in s:
+        if 'a' <= char <= 'z':
+            print("{}".format(chr(ord(char) - ord('a') + ord('A'))), end="")
+        else:
+            print("{}".format(char), end="")
+    print()
