b9076bc44aa6e074218e0a6c7c3322408149a7ca
root <suhailyksa@gmail.com>
2023-08-30 14:08:30 -0700

    error and execeptoins

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))  # Print the integer value
+    except ValueError as ve:
+        print("Exception: {}".format(ve), file=sys.stderr)
+        return False  # An exception ocand return False
+    else:
+        return True  # No exception occurred, so value is an integer
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)  # Call the provided functi given arguments
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None  # An exception occurred, print then None
+    else:
+        return result  # No exception occurred, return 
