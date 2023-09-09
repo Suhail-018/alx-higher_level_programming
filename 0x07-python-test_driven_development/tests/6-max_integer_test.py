1fb9e6e345772cbde563be470d9a58664815a766
root <suhailyksa@gmail.com>
2023-09-09 14:18:43 -0700

    test driven development python project

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,8 +9,12 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         
         self.assertEqual(max_integer([1, 3, 4]), 4)
-        self.assertEqual(max_integer([1, -7, 9 ]), 9)
+        self.assertEqual(max_integer([9, 1, -7]), 9)
         self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([1, 6, 5]), 5)
+        self.assertEqual(max_integer([-1]), -1)
+        self.assertEqual(max_integer([-2, -3, -9]), -2)
+        self.assertEqual(max_integer([8]), 8)
 
 
 if __name__ == '__main__':
