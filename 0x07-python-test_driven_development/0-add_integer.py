6326130b80ca6cd5bd44d538d98df7dd1bc30d07
suhail-018 <suhailyksa@gmail.com>
2023-11-02 02:21:11 +0000

    test uyor owncode

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -33,9 +33,10 @@ def add_integer(a, b=98):
         >>> add_integer(100.3, -2)
         98
     """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
 
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -55,6 +55,6 @@ def print_square(size):
 
     if size < 0:
         raise ValueError("size must be >= 0")
-
+  
     for _ in range(size):
         print("#" * size)
