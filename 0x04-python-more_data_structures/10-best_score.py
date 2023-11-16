ad8c775912a5b63ac2824c143442e0304039f712
Suhail-018 <107703767+Suhail-018@users.noreply.github.com>
2023-11-17 00:18:45 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
-i#!/usr/bin/python3
-
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
-        return None
-    
+        return None    
     best_key = max(a_dictionary, key=a_dictionary.get)
     return best_key
