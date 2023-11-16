2b627f61cfbf5f83ea4bfcb736c8e56b5f25553d
Suhail-018 <107703767+Suhail-018@users.noreply.github.com>
2023-11-16 23:55:02 +0300

    more dictionary ,tuple and set

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
-
-# Function to print a sorted dictionary
-def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-    for key in sorted_keys:
-        print("{}: {}".format(key, a_dictionary[key]))
