3744aff2b64ceb966b096a2708b58b1b7b54a869
Suhail-018 <107703767+Suhail-018@users.noreply.github.com>
2023-12-03 19:36:57 +0300

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,10 @@ def safe_print_list_integers(my_list=[], x=0):
 
     try:
         for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end='')  # Print integers only
-                printed += 1
+            if my_list[i]:
+                if isinstance(my_list[i], int):
+                    print("{:d}".format(my_list[i]), end='')  # Print integers only
+                    printed += 1
     except (IndexError, TypeError):
         pass  # H and TypeError (non-integer)
 
