c7d80e891815089b628c1cdf08b727fda8a8408e
Suhail-018 <107703767+Suhail-018@users.noreply.github.com>
2023-12-07 16:45:12 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+The add_item_managerencapsulatesfunctionalitymanaging a list$savingto a JSON file.
+"""
 import sys
 from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -6,6 +9,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_item():
+     """
+     Manage a list, add command-line arguments, and save the updated list to a JSON file.
+     Parameters:
+     None
+     Returns:
+     None
+     """
     # Check if the add_item.json file exists
     if exists("add_item.json"):
         # Load the existing list from the file
