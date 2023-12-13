22fe38abd94b2efb15d1c3db19d023b9a26738d9
suhail-09 <you@example.com>
2023-12-13 14:53:19 -0800

    0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- 13-count_shows_by_genre.sql
+
+-- Use the hbtn_0d_tvshows database
+USE hbtn_0d_tvshows;
+
+-- Select genre and count the number of shows linked to each genre
+SELECT tv_genres.genre AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.genre
+ORDER BY number_of_shows DESC;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,10 @@
+-- 9-cities_by_state_join.sql
+
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Select required columns and join cities and states tables
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
