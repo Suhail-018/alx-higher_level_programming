68c9ae242a01de9d0476440d37b6cf0e85274234
Suhail-018 <suhailyksa@gmail.com>
2024-01-09 19:09:22 -0800

    0x13. JavaScript - Objects, Scopes and Closures JavaScript

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
+// 2-rectangle.js
+
 class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {}; // Create an empty object
+      // Initialize width and height to undefined if w or h is 0 or not a positive integer
+      this.width;
+      this.height;
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
